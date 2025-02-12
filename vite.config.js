a32fa254b344fa536d7ef7a7ab8e344dfe2908fd
SoNewGameRB <stupidlin11@gmail.com>
2025-02-12 15:08:41 +0800

    第二次

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/bookkee/",  // 請確保這是你的 GitHub Repository 名稱
   plugins: [vue()],
-})
+});
