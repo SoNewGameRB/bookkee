b2540c8b195e13ee63b51a3b326a8a495e5a52eb
SoNewGameRB <stupidlin11@gmail.com>
2025-02-28 17:07:18 +0800

    更改vite 變數

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/bookkee/",  // 請確保這是你的 GitHub Repository 名稱
   plugins: [vue()],
+  base: './', // ✅ 確保與 GitHub Pages 網址對應
 });
